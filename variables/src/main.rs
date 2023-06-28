02635188ee518b606b60a754d803fbbf5b13eb94
Lorcan Rae <lorcanrae@outlook.com>
2023-06-28 22:09:45 +0100

    bools and characters

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,19 +1,28 @@
 fn main() {
-    let x = 5;
+    // let x = 5;
 
-    let x = x + 1;
+    // // Overshadowing previous instance of x
+    // let x = x + 1;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
 
-    println!("The value of x is: {x}");
+    // println!("The value of x is: {x}");
 
 
-    let mut y = 5;
-    println!("The value of y is: {y}");
+    // // The following works because y is the same data type
+    // // The reassignment wouldn't work if y was reassigned to a different type.
+    // let mut y = 5;
+    // println!("The value of y is: {y}");
 
-    y = 10;
+    // y = 10;
+    // println!("The value of y is: {y}");
+
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
     println!("The value of y is: {y}");
 }
